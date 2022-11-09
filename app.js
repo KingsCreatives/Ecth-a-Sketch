ef8137130b6467045e5a460e3dc1cdfc74332b2f
kwamecody <kwamecody@gmail.com>
2022-11-09 16:39:47 +0000

    function that creates 16 X 16 grid

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("it works");
+const wrapper = document.querySelector(".grid-wrapper");
+
+
+function createGrid(num) {
+    for (var rows = 0; rows < num; rows++) {
+        for (var columns = 0; columns < num; columns++) {
+            const grid = document.createElement("div");
+            grid.className = "grid";
+            wrapper.append(grid);
+
+            grid.style.width = parseInt(480) / num + "px";
+            grid.style.height = parseInt(480) / num + "px";
+        }
+    }
+
+}
+
+createGrid(16);
