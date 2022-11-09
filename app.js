9535aed2da62ff2e59a8ced5daa777a2022a0182
kwamecody <kwamecody@gmail.com>
2022-11-09 18:00:36 +0000

    selected buttons

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-const wrapper = document.querySelector(".grid-wrapper");
+const wrapper = document.querySelector('.grid-wrapper');
+const gridDiv = document.getElementsByClassName('grid');
+const newGridBtn = document.querySelector('#new-grid');
+const randomColor = document.querySelector('.random-color');
+const clearBtn = document.querySelector('.clear-btn');
+const colorSelector = document.querySelector('#color-selector');
 
-
-function createGrid(num) {
+// Create 12 x 12 grid by default
+function createGrid(num = 12) {
     for (var rows = 0; rows < num; rows++) {
         for (var columns = 0; columns < num; columns++) {
-            const grid = document.createElement("div");
-            grid.className = "grid";
+            const grid = document.createElement('div');
+            grid.className = 'grid';
             wrapper.append(grid);
 
-            grid.style.width = parseInt(480) / num + "px";
-            grid.style.height = parseInt(480) / num + "px";
+            grid.style.width = parseInt(480) / num + 'px';
+            grid.style.height = parseInt(480) / num + 'px';
+
+            grid.onmouseover = function() {
+                grid.style.backgroundColor = 'black';
+            };
         }
     }
-
 }
 
-createGrid(16);
+// clear window
+clearBtn.addEventListener('click', function() {
+    window.location.reload();
+});
